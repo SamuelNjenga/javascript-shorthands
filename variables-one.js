0678984cc849a1ee5560a4b1aa5718e8b3e91e89
Samuel Njenga <snjenga704@gmail.com>
2021-09-13 14:34:36 +0300

    Added variables files

---
+++ variables-one.js
@@ -0,0 +1,10 @@
+// Long version  
+let a;   
+let b = 1; 
+//console.log(a);
+//console.log(b);
+
+// Shorthand Version -> Easily declare / define multiple variables on one line 
+let a, b = 1; 
+console.log(a);
+console.log(b);
---
+++ variables-two.js
@@ -0,0 +1,11 @@
+//Long version
+let x = 1
+let y = 2
+let z = 3
+
+// console.log(x, y, z)
+// console.log(typeof x)
+
+//Shorthand Version -> Assign values to multiple variables with array destructuring
+let [x, y, z] = [1, 2, 3]
+console.log(x, y, z)
