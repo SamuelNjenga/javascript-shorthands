1c7a64aeb376755d6b2a671df05ac979a3d7f4e0
Samuel Njenga <snjenga704@gmail.com>
2021-09-13 15:04:01 +0300

    Added template-literal.js

---
+++ template-literal.js
@@ -0,0 +1,8 @@
+const name = 'JavaScript'
+const day = 'Monday'
+
+//Long Version
+console.log('Hello '+ name+', it is '+day)
+
+//Shorthand Version -> instead of using the + operator to concatenate strings we can use ES6 template literals
+console.log(`Hello ${name}, it is ${day}`);
